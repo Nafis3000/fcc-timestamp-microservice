b4f17bab7c5509302f8e7ab9e07ec4d07e0247ae
Nafis.3000 <nafis.3000@gmail.com>
2023-03-28 15:00:14 -0600

    timestamp microservice

--- index.js
+++ index.js
@@ -24,9 +24,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function (req, res) {
+  res.json({unix: new Date().getTime(), utc: new Date().toUTCString()})
+})
 
+app.get("/api/:timestamp", function(req, res) {
+  let timestamp = req.params.timestamp;
 
+  if (!isNaN(Number(timestamp)) && timestamp.length === 13) {
+    return res.json({
+      unix: timestamp,
+      utc: new Date(parseInt(timestamp)).toUTCString()
+    })
+  }
+
+  if (new Date(timestamp).toUTCString() !== "Invalid Date"){
+    return res.json({
+      unix: new Date(timestamp).getTime(),
+      utc: new Date(timestamp).toUTCString()
+    })
+  } 
+
+  res.send({error : "Invalid Date"});
+})
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + 3000);
 });
